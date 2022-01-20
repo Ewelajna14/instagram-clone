d01868136ea6188247c44cb3332f3d6c1338cc0f
Ewelina <ekowalczyk1994@gmail.com>
2022-01-20 16:54:12 -0600

    state was defined

--- phase-3-react/src/Components/App.js
+++ phase-3-react/src/Components/App.js
@@ -3,7 +3,9 @@ import '../App.css';
 import Header from './Header';
 import PostContainer from './PostContainer';
 
+
 function App() {
+
   return (
     <div className="app">
       <Header/>
--- phase-3-react/src/Components/PostContainer.js
+++ phase-3-react/src/Components/PostContainer.js
@@ -1,10 +1,27 @@
 
 import Post from './Post'
+import React, {useState, useEffect} from 'react' 
 
 function PostContainer(){
+    const [posts, setPosts] = useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:9292/posts")
+        .then(r=>r.json())
+        .then(posts=>setPosts(posts))
+    },[])
+
+    let displayPosts = posts.map((post)=>{
+        return(
+        <Post key={post.id} post={post}/>
+        )
+    })
+
+    console.log(posts)
+
     return(
         <div>
-            <Post/>
+            {displayPosts}
         </div>
 
     )
