d31f8576188a2ffb4533c8899b3f954f4c96aa86
Ewelina <ekowalczyk1994@gmail.com>
2022-01-20 15:23:16 -0600

    seeding data

--- phase-3-sinatra-project/db/seeds.rb
+++ phase-3-sinatra-project/db/seeds.rb
@@ -1,5 +1,20 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding users..."
 
-# Seed your database here
+#run a loop 8 times
+8.times do
+user = User.create(
+    name: Faker::Name.name,
+    avatar: "/static/images/avatar/1.jpg"
+)
+
+# create between 1 and 10 posts for each user
+rand(1..10).times do 
+    Post.create(
+        image_url: Faker::LoremFlickr.image,
+        description: Faker::Lorem.sentence,
+        user_id: user.id
+    )
+end
+end
 
 puts "✅ Done seeding!"
