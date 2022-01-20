536136ff3f3c1494e639e2f5bf7c1aa73cc4aed8
Ewelina <ekowalczyk1994@gmail.com>
2022-01-20 09:33:53 -0600

    postContainer and Post was created

--- phase-3-react/src/Components/App.js
+++ phase-3-react/src/Components/App.js
@@ -4,12 +4,12 @@ import '../App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
-
       <div className="header"> 
        <img className="header-image"
        src="Instagram-Wordmark-Logo.wine.svg" alt="logo"/>
       </div>
+      <h1>Hello</h1>
+
     </div>
   );
 }
