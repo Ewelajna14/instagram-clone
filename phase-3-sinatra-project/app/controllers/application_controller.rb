f78306118cf61040101886f05dece8dc3e6c2e1b
Ewelina <ekowalczyk1994@gmail.com>
2022-01-21 19:09:13 -0600

    controller post was created

--- phase-3-sinatra-project/app/controllers/application_controller.rb
+++ phase-3-sinatra-project/app/controllers/application_controller.rb
@@ -2,9 +2,28 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+
+  get "/" do 
+    "Hello"
+  end
+  
   get "/posts" do
     posts = Post.all
     posts.to_json(include: {comments:{include: :user}})
+  end
+
+  get "/posts/comments" do
+    comments = Comment.all
+    comments.to_json(include: :user)
+  end
+
+  post "/posts/comments" do
+    comment = Comment.create(
+    content: params[:content],
+    user_id: params[:user_id],
+    post_id: params[:post_id]
+    )
+    comment.to_json
   end
 
 end
